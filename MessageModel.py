6ce1c2ffd3469d9e57fedaa4e8bc306c272131da
jorsae <jorgen.sater@gmail.com>
2020-11-07 15:17:18 +1100

    CAn now log discord messages

--- MessageModel.py
+++ MessageModel.py
@@ -1,11 +1,15 @@
 from peewee import *
 import datetime
-
 from BaseModel import BaseModel
+from ChannelModel import ChannelModel
 
-class DiscordChannel(BaseModel):
-    channel_id = PrimaryKeyField()
+class MessageModel(BaseModel):
+    message_id = PrimaryKeyField()
     author = TextField()
     is_bot = BooleanField(default=False)
     message_content = TextField()
     created_date = DateTimeField(default=datetime.datetime.now)
+    channel_id = ForeignKeyField(ChannelModel, to_field='channel_id')
+    
+    class Meta:
+        db_table = 'Messages'
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from peewee import *
 import discord
 import os
+import datetime
 import logging
 
 import settings
@@ -8,17 +9,38 @@ import constants
 from BaseModel import BaseModel, database
 from ChannelModel import ChannelModel
 from ServerModel import ServerModel
-from DiscordChannel import DiscordChannel
+from MessageModel import MessageModel
 
 client = discord.Client()
 settings = settings.Settings('settings.json')
 
 @client.event
 async def on_message(message):
-    print(f'[{message.guild.name}] #{message.channel} - ({message.author}): {message.content}')
+    print(f'[{message.guild.name}] #{message.channel} - ({message.author}[{message.author.bot}]): {message.content}')
     
     server, created = ServerModel.get_or_create(server=message.guild.name)
     channel, created = ChannelModel.get_or_create(channel=message.channel, server_id=server.server_id)
+    
+    """
+        message_id = PrimaryKeyField()
+        author = TextField()
+        is_bot = BooleanField(default=False)
+        message_content = TextField()
+        created_date = DateTimeField(default=datetime.datetime.now)
+        channel_id = ForeignKeyField(ChannelModel, to_field='channel_id')
+    """
+    table_name = f'{server.server_id}_{message.channel}'.replace('-', '_')
+    message_table = type(table_name, (MessageModel,), {})
+    database.create_tables([message_table])
+    insert_message(table_name, str(message.author), message.author.bot, message.content, message.created_at, channel.channel_id)
+
+def insert_message(table_name, author, is_bot, content, created_at, channel_id):
+    query = f"""INSERT INTO '{table_name}' (author, is_bot, message_content, created_date, channel_id)
+                VALUES (?, ?, ?, ?, ?)"""
+    values = (author, is_bot, content, created_at, channel_id, )
+    print(query)
+    print(values)
+    database.execute_sql(query, values)
 
 def setup_database():
     database.create_tables([ChannelModel, ServerModel])
