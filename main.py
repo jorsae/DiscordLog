9304deab213c738f1a7100883b43538c5c39975f
jorsae <jorgen.sater@gmail.com>
2020-11-07 12:09:55 +1100

    Added database and log file in settings

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import settings
 
 settings = settings.Settings('settings.json')
 
+
+def setup_logging():
+    logFolder = 'logs'
+    logFile = 'muda.log'
+    if not os.path.isdir(logFolder):
+        os.makedirs(logFolder)
+
+    logging.basicConfig(filename=f'{logFolder}/{logFile}', level=logging.INFO, format='%(asctime)s %(levelname)s:[%(filename)s:%(lineno)d] %(message)s')
+
 if __name__ == '__main__':
     settings.parse_settings()
     settings.print_settings()
--- settings.py
+++ settings.py
@@ -5,18 +5,22 @@ class Settings():
     def __init__(self, settings_file):
         self.settings_file = settings_file
         self.token = None
-        self.run_bot = True
+        self.database_file = 'DiscordDatabase.db'
+        self.log_file = 'DiscordLog.log'
     
     def print_settings(self):
         token = 'Not set' if self.token is None else 'set'
         print(f'settings_file: {self.settings_file}')
         print(f'Token: {token}')
+        print(f'database_file: {self.database_file}')
+        print(f'log_file: {self.log_file}')
 
     def parse_settings(self):
         try:
             with open(self.settings_file, 'r', encoding='utf-8') as f:
                 data = json.loads(f.read())
             self.token = data.get("token")
+            self.database = data.get("database")
             
             logging.info('Settings were parsed successfully')
             return True
