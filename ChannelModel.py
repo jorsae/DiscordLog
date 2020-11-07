44c7ee6839602b2e76549df9ec9599b874cad8ce
jorsae <jorgen.sater@gmail.com>
2020-11-07 17:04:22 +1100

    Now tracks if channels change name

--- ChannelModel.py
+++ ChannelModel.py
@@ -6,10 +6,8 @@ from ServerModel import ServerModel
 class ChannelModel(BaseModel):
     channel_id = PrimaryKeyField()
     channel = TextField()
+    channel_name = TextField()
     created_date = DateTimeField(default=datetime.datetime.now)
     server_id = ForeignKeyField(ServerModel, to_field="server_id")
     class Meta:
-        indexes = (
-            (("channel", "server_id"), True),
-        )
         db_table = 'Channels'
--- main.py
+++ main.py
@@ -24,21 +24,37 @@ async def on_message(message):
         return
 
     print(f'[{message.guild.name}] #{message.channel} - ({message.author}[{message.author.bot}]): {message.content}')
-
-    try:
-        server = ServerModel.select().where(ServerModel.server == str(message.guild.id)).order_by(ServerModel.created_date.desc()).get()
-    except DoesNotExist:
-        server, _ = ServerModel.get_or_create(server=str(message.guild.id), server_name=message.guild.name)
-        logging.info(f'New channel: ({server.server_id}) {server.server} added')
-    except Exception as e:
-        logging.critical(f'Error when trying to get most recent server: {e}')
     
+    server = get_server(message)
+    channel = get_channel(server, message)
+
     # If guild.name is not the same as last guild.name, insert new row
     if server.server_name != message.guild.name:
         ServerModel.create(server=str(message.guild.id), server_name=message.guild.name)
     
-    channel, _ = ChannelModel.get_or_create(channel=message.channel, server_id=server.server_id)
     MessageModel.create(author=str(message.author), is_bot=message.author.bot, message_content=message.content, channel_id=channel.channel_id)
+
+def get_channel(server, message):
+    try:
+        channel = ChannelModel.select().where(ChannelModel.server_id == server.server_id, ChannelModel.channel_name == server.server_name).get()
+        return channel
+    except DoesNotExist:
+        channel, _ = ChannelModel.get_or_create(channel=message.channel.id, channel_name=message.channel, server_id=server.server_id)
+        logging.info(f'New channel: {message.channel} added')
+        return channel
+    except Exception as e:
+        logging.critical(f'get_channel: {e}')
+
+def get_server(message):
+    try:
+        server = ServerModel.select().where(ServerModel.server == str(message.guild.id)).order_by(ServerModel.created_date.desc()).get()
+        return server
+    except DoesNotExist:
+        server, _ = ServerModel.get_or_create(server=str(message.guild.id), server_name=message.guild.name)
+        logging.info(f'New server: ({server.server_id}) {server.server} added')
+        return server
+    except Exception as e:
+        logging.critical(f'get_server: {e}')
 
 def setup_database():
     database.create_tables([ChannelModel, ServerModel, MessageModel])
